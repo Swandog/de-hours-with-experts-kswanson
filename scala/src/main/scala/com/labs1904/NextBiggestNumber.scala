44b338e9bf317c54dfb32d5e95453aaf266564a1
Kenneth Swanson <kswanson@pop-os.localdomain>
2021-02-22 22:43:39 -0600

    Found cases where it can't be done

--- scala/src/main/scala/com/labs1904/NextBiggestNumber.scala
+++ scala/src/main/scala/com/labs1904/NextBiggestNumber.scala
@@ -11,7 +11,30 @@ object NextBiggestNumber {
   }
 
   def getNextBiggestNumber(i: Integer) = {
-    //TODO: Implement me!
-    0
+    // The next biggest number will be found by rearranging
+    // the longest sequence of declining digits that includes the final digit
+    // and the digit just before that
+
+    // First, split the number into digits
+    val digits = i.toString().toList.map(_.asDigit)
+
+    // reverse the sequence of digits
+    val digitsReversed = digits.reverse
+
+    // find the spot where the revesed digits stop increasing
+    val pivotPoint = digitsReversed.sliding(2).indexWhere(x => x(0) > x(1)) + 1
+
+    if(pivotPoint > 0) {
+      // The pivot point is the start of the digits we want to change
+      // Split the (reversed) list into numbers not changing, and those that are
+      val (changing, pivot :: reversedUnchanged) = digitsReversed.splitAt(pivotPoint)
+
+      0
+    } else {
+      // If the pivotPoint is 0, that means indexWhere returned -1, which means
+      // the revesed digits only increased, which means that the original sequence
+      // only decreased. In this case, a higher number cannot be found
+      -1
+    }
   }
 }
